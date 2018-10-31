b2c3b96a71707082dc03e085c64a19b0bd1d96ba
igorbash <igor731996@gmail.com>
2018-10-31 16:47:02 +0200

    complete issue #30

---
+++ src/main/java/tests/TestEmpty.java
@@ -0,0 +1,25 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.jetbrains.annotations.*;
+import org.junit.*;
+
+import an.*;
+
+public class TestEmpty {
+  @Test public void testEmptyList() {
+    List<Object> testList = empty.list();
+    
+    assertEquals(0, testList.size());
+  }
+  
+  @Test public void testEmptyIterable() {
+    Iterator<Object> testIterable = empty.iterable().iterator();
+    
+    assertEquals(Boolean.valueOf(false), Boolean.valueOf(testIterable.hasNext()));
+    assertEquals(null, testIterable.next());
+  }
+}
