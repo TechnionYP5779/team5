6c935ebd4fafffe5039ab620bda1a35a8f7378b5
shaked-sapir <shaked.sapir@gmail.com>
2018-11-03 18:27:43 +0200

    tests for Range,Pair,Int in .Utils

---
+++ src/test/java/il/org/spartan/utils/IntTest.java
@@ -0,0 +1,96 @@
+package il.org.spartan.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class IntTest {
+  @Test public void test_constrAndValueOf() {
+    Int i = new Int(10);
+    assertEquals(10,i.get());
+    Int i2 = i.valueOf(9);
+    assertEquals(9,i2.get());
+  }
+  
+  @Test public void test_inner() {
+    Int i1 = new Int(10);
+    Integer inner = i1.inner(), inner2 = i1.inner();
+    assertEquals(10,inner.intValue());
+    assertEquals(10,inner2.intValue());
+    assert(inner.equals(inner2));
+  }
+  
+  @Test public void test_stepAndGet() {
+    Int i = new Int(0);
+    for(int x = 1;x<=10;x++) {
+      i.step();
+      assertEquals(x,i.get());
+    }
+  }
+  
+  @Test public void test_add() {
+    Int i = new Int(0);
+    int arr[] = {1,3,6,10,15,21,28,36,45,55};
+    for(int x=1;x<=10;x++) {
+      i.add(x);
+      assertEquals(arr[x-1],i.get());
+    }
+    for(int x=10;x>=2;x--) {
+      i.add(-x);
+      assertEquals(arr[x-2],i.get());
+    }
+  }
+  
+  @Test public void test_set() {
+    Int i = new Int (0);
+    assertEquals(0,i.get());
+    for(int x=1;x<=10;x++) {
+      i.set(x);
+      assertEquals(x,i.get());
+    }
+  }
+  
+  @Test public void test_toString() {
+    Int i = new Int(0);
+    for(int x=1;x<=10;x++) {
+      i.set(x);
+      assertEquals(x+"",i.toString());
+      assertEquals(""+x,i.toString());
+      assertEquals(""+x+"",i.toString());
+    }
+  }
+  
+  @Test public void test_nextAndGet() {
+    Int i = new Int(0);
+    for(int x = 1;x<=10;x++) {
+      int d = i.next();
+      assertEquals(x,d);
+    }
+  }
+  
+  @Test public void test_add2() {
+    Int i = new Int(0);
+    int arr[] = {1,3,6,10,15,21,28,36,45,55};
+    for(int x=1;x<=10;x++) {
+      Int i2 = new Int(x);
+      i.add(i2);
+      assertEquals(arr[x-1],i.get());
+    }
+    for(int x=10;x>=2;x--) {
+      Int i2 = new Int(-x);
+      i.add(i2);
+      assertEquals(arr[x-2],i.get());
+    }
+  }
+  
+  @Test public void test_clear() {
+    Int i = new Int(10);
+    assertEquals(10,i.get());
+    i.clear();
+    assertEquals(0,i.get());
+    i.add(0);
+    int old = i.get();
+    i.clear();
+    assertEquals(old,i.get());
+  }
+}
---
+++ src/test/java/il/org/spartan/utils/RangeTest.java
@@ -0,0 +1,109 @@
+package il.org.spartan.utils;
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.junit.*;
+
+public class RangeTest {
+  @Test public void test_cnstr() {
+    Range r = new Range(0,10);
+    assertEquals(0,r.from);
+    assertEquals(10,r.to);
+     Range r2 = new Range(r);
+     assertEquals(0,r2.from);
+     assertEquals(10,r2.to);
+  }
+  
+  @Test public void test_equals() {
+    Range r1 = new Range(0,10);
+    Range r2 = new Range(10,20);
+    Range r3 = new Range(r2);
+    String str = new String("hello");
+    assert(r1.equals(r1));
+    assert(r2.equals(r3));
+    assert(!r1.equals(r3));
+    assert(!r1.equals(str));
+  }
+  
+  @Test public void test_findIbcludedIn() {
+    Range r1 = new Range(0,4);
+    ArrayList<Range> list = new ArrayList<>();
+    for(int i=0;i<4;i++) {
+      list.add(new Range(0,i));
+      assertEquals(null,r1.findIncludedIn(list));
+    }
+    for(int i=9;i>=4;i--) {
+      ArrayList<Range> l2 = new ArrayList<>();
+      Range r2 = new Range(0,i);
+      l2.add(r2);
+      assertEquals(r2,r1.findIncludedIn(l2));
+    }
+  }
+  
+  @Test public void test_isEmpty() {
+    Range r1 = new Range(0,10), r2 = new Range(0,0), r3 = new Range(-1,14), r4 = new Range(1,0), r5 = new Range(9,9);
+    assert(!r1.isEmpty());
+    assert(r2.isEmpty());
+    assert(!r3.isEmpty());
+    assert(r4.isEmpty());
+    assert(r5.isEmpty());
+  }
+  
+  @Test public void test_merge() {
+    Range r1 = new Range(0,10);
+    Range r2 = r1.merge(new Range(-1,11));
+    Range r3 = r1.merge(new Range(-1,9));
+    Range r4 = r1.merge(new Range(1,15));
+    
+    assertEquals(-1,r2.from);
+    assertEquals(-1,r3.from);
+    assertEquals(0,r4.from);
+    assertEquals(11,r2.to);
+    assertEquals(10,r3.to);
+    assertEquals(15,r4.to);
+  }
+  
+  @Test public void test_overlapping() {
+    /**
+     *  public boolean overlapping(final @NotNull Range ¢) {
+    return from >= ¢.from || to <= ¢.to;
+  }
+     */
+    Range r1 = new Range(0,10);
+    Range r2 = new Range(0,11), r3 = new Range(-1,15), r4 = new Range(-4,9), r5 = new Range(1,9);
+    assert(r1.overlapping(r2));
+    assert(r1.overlapping(r3));
+    assert(r1.overlapping(r4));
+    assert(!r1.overlapping(r5));
+    assert(r5.overlapping(r1)); // fadiha
+  }
+  
+  @Test public void test_pruneIncluders() {
+    Range r1 = new Range(0,4);
+    ArrayList<Range> list = new ArrayList<>();
+    for(int i=0;i<10;i++) {
+      list.add(new Range(0,i));
+    }
+    assertEquals(10,list.size());
+    r1.pruneIncluders(list);
+    assertEquals(4,list.size());
+  }
+  
+  @Test public void test_size() {
+    for(int i=0;i<10;i++) {
+      Range r = new Range(0,i), r2 = new Range(i,0);
+      assertEquals(i,r.size());
+      assertEquals(-i,r2.size());
+    }
+  }
+  
+  @Test public void test_toString() {
+    for(int i=0;i<10;i++) {
+      Range r = new Range(0,i), r2 = new Range(i,0);
+      assertEquals("["+0+", "+i+"]",r.toString());
+      assertEquals("["+i+", "+0+"]",r2.toString());
+    }
+  }
+}
