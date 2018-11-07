b4a7f758665a2a287fcbf227354e405aa38acef6
LiorBenYamin <lior.benyamin7@gmail.com>
2018-11-08 00:13:04 +0200

    worked 1 hour on issue #68

---
+++ src/test/java/il/org/spartan/UtilsTest.java
@@ -0,0 +1,80 @@
+package il.org.spartan;
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.junit.*;
+
+import fluent.ly.*;
+
+public class UtilsTest {
+  final static Integer i = null;
+  
+  @SuppressWarnings("static-method") @Test public void addTest() {
+    ArrayList<Integer> a = new ArrayList<>();
+    a.add(Integer.valueOf(5));
+    Utils.add(a, Integer.valueOf(5));
+    azzert.that(a.size(), azzert.is(2));
+    ArrayList<Integer> b = new ArrayList<>();
+    b.add(null);
+    b.add(Integer.valueOf(5));
+    Utils.add(a, b);
+    azzert.that(a.size(), azzert.is(3));
+    Utils.add(a, i);
+    azzert.that(a.size(), azzert.is(3));
+  }
+  
+  static Iterable<Integer> range(final int from, final int to) {
+    return new Iterable<Integer>() {
+        @Override public Iterator<Integer> iterator() {
+            return new Iterator<Integer>() {
+                int current = from;
+                @Override public boolean hasNext() { return current < to; }
+                @SuppressWarnings("boxing") @Override public Integer next() {
+                    return current++;
+                }
+            };
+        }
+    };
+}
+  
+  
+  @SuppressWarnings("static-method") @Test public void addAllTest() {
+    ArrayList<Integer> a = new ArrayList<>();
+    ArrayList<Integer> b = new ArrayList<>();
+    b.add(Integer.valueOf(5));
+    azzert.that(a.size(), azzert.is(0));
+    Utils.addAll(a, b);
+    azzert.that(a.size(), azzert.is(1));
+    Utils.addAll(a, Integer.valueOf(5));
+    azzert.that(a.size(), azzert.is(2));
+    Utils.addAll(a, i);
+    azzert.that(a.size(), azzert.is(2));
+    Utils.addAll(a, range(3,5));
+    azzert.that(a.size(), azzert.is(4));
+  }
+  
+  @SuppressWarnings("static-method") @Test public void appendTest() {
+    Integer[] arr = new Integer[10];
+    azzert.that(Utils.append(arr,Integer.valueOf(5))[10], azzert.is(5));
+  }
+  
+  @SuppressWarnings("static-method") @Test public void applyTest() {
+    // TO DO - test
+  }
+  
+  @SuppressWarnings("static-method") @Test public void canBeNullTest() {
+    azzert.that(Utils.canBeNull(Integer.valueOf(5)), azzert.is(5));
+  }
+  
+  @SuppressWarnings("static-method") @Test public void cantBeNullTest() {
+    azzert.that(Utils.cantBeNull(Integer.valueOf(5)), azzert.is(5));
+  }
+  
+  @SuppressWarnings("static-method") @Test public void compareTest() {
+    
+  }
+  
+  
+}
