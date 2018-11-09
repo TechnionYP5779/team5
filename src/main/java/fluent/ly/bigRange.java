9df10479ca5dc141cf72f1e051146606266ae5b3
אור פלדמן <orfeldman@campus.technion.ac.il>
2018-11-09 13:57:53 +0200

    implementing and testing check box 5,6 and 7 of issue #80

--- src/main/java/fluent/ly/bigRange.java
+++ src/main/java/fluent/ly/bigRange.java
@@ -6,30 +6,34 @@ import java.util.*;
 public class bigRange {
 
   public static theRange withDiff(long i) {
-   return new theRange(){
-
-    @Override public Iterator<BigInteger> iterator() {
-      return new Iterator<BigInteger>() {
-
-        private BigInteger current=BigInteger.valueOf(0-i);
-        
-        @Override public boolean hasNext() {
-          return true;
-        }
-
-        @Override public BigInteger next() {
-          current=current.add(BigInteger.valueOf(i));
-          return current;
-        }
-      };
-    }
-    @Override public BigInteger Sum() {
-      if(i!=0)
-        return null;
-      return BigInteger.valueOf(0);
-    }
-   };
+   return withDiff(BigInteger.valueOf(i));
   }
+  
+  public static theRange withDiff(BigInteger i) {
+    return new theRange(){
+
+     @Override public Iterator<BigInteger> iterator() {
+       return new Iterator<BigInteger>() {
+
+         private BigInteger current=BigInteger.valueOf(0).subtract(i);
+         
+         @Override public boolean hasNext() {
+           return true;
+         }
+
+         @Override public BigInteger next() {
+           current=current.add(i);
+           return current;
+         }
+       };
+     }
+     @Override public BigInteger Sum() {
+       if(i.compareTo(BigInteger.valueOf(0))!=0)
+         return null;
+       return BigInteger.valueOf(0);
+     }
+    };
+   }
   
   public interface theRange extends Iterable<BigInteger>{
     public  BigInteger Sum();
@@ -37,64 +41,88 @@ public class bigRange {
 
   
   public static class afterFrom{
-    final long first;
-    final long last;
+    final BigInteger first;
+    final BigInteger last;
     final boolean isFinit;
-    public afterFrom(long first) {
-      this.first=first;
-      last=0;
+    public afterFrom(BigInteger first) {
+      last=BigInteger.valueOf(0);
+      this.first=first.add(last);
       isFinit=false;
     }
     
-    public afterFrom(long f,long l) {
+    public afterFrom(BigInteger f,BigInteger l) {
       first=f;
       last=l;
       isFinit=true;
     }
     
     public  theRange withDiff(long i) {
-      return new theRange(){
-
-       @Override public Iterator<BigInteger> iterator() {
-         return new Iterator<BigInteger>() {
-
-           private BigInteger current=BigInteger.valueOf(first-i);
-           private BigInteger l=BigInteger.valueOf(last);
-           
-           @Override public boolean hasNext() {
-             return !isFinit|| current.compareTo(l)<0;
-           }
-
-           @Override public BigInteger next() {
-             if(hasNext())
-               current=current.add(BigInteger.valueOf(i));
-             return current;
-           }
-           
-         };
-       }
-
-      @Override public BigInteger Sum() {
-        if(!isFinit)
-          return null;
-        BigInteger f=BigInteger.valueOf(first);
-        BigInteger l=BigInteger.valueOf(last);
-        BigInteger s=BigInteger.valueOf(i);
-        BigInteger n=l.subtract(f).divide(s).add(BigInteger.valueOf(1));
-        return n.multiply(f.multiply(BigInteger.valueOf(2)).add(s.multiply(n.subtract(BigInteger.valueOf(1))))).divide(BigInteger.valueOf(2)); 
-      }
-        
-      };
+      return withDiff(BigInteger.valueOf(i));
      }
 
     public afterFrom to(long i) {
+      return new afterFrom(first,BigInteger.valueOf( i));
+    }
+    
+    public afterFrom to(BigInteger i) {
       return new afterFrom(first,i);
     }
-  }
+
+    public theRange withDiff(BigInteger i) {
+      if(isFinit&&((last.compareTo(first)==1 && i.compareTo(BigInteger.valueOf(0))<0)||(first.compareTo(last)==1 && i.compareTo(BigInteger.valueOf(0))>0)))
+        return null;
+    return  new theRange(){
+
+     @Override public Iterator<BigInteger> iterator() {
+       return new Iterator<BigInteger>() {
+
+         private BigInteger current=first.subtract(i);
+         
+         @Override public boolean hasNext() {
+           return !isFinit|| current.compareTo(last)<0;
+         }
+
+         @Override public BigInteger next() {
+           if(hasNext())
+             current=current.add(i);
+           return current;
+         }
+         
+       };
+     }
+
+    @Override public BigInteger Sum() {
+      if(!isFinit)
+        return null;
+      BigInteger f=first;
+      BigInteger l=last;
+      BigInteger s=i;
+      BigInteger n=l.subtract(f).divide(s).add(BigInteger.valueOf(1));
+      return n.multiply(f.multiply(BigInteger.valueOf(2)).add(s.multiply(n.subtract(BigInteger.valueOf(1))))).divide(BigInteger.valueOf(2)); 
+    }
+      
+    };
+   }
+    }
   
   public static afterFrom from(long i) {
+    return new afterFrom(BigInteger.valueOf(i));
+  }
+  
+  public static afterFrom from(BigInteger i) {
     return new afterFrom(i);
   }
 
+  public static afterFrom to(long i) {
+    return new afterFrom(BigInteger.valueOf(0),BigInteger.valueOf(i));
+  }
+  
+  public static afterFrom to(BigInteger i) {
+    return new afterFrom(BigInteger.valueOf(0),i);
+  }
+  
+  public static theRange naturals() {
+    return withDiff(1);
+  }
   
 }
--- src/test/java/fluent/ly/bigRangeTest.java
+++ src/test/java/fluent/ly/bigRangeTest.java
@@ -5,7 +5,7 @@ import java.math.*;
 import org.junit.*;
 
 public class bigRangeTest {
-  @SuppressWarnings({ "static-method", "static-access" }) @Test public void withDiffOnly() {
+  @SuppressWarnings({ "static-method" }) @Test public void withDiffOnly() {
     final Iterable<BigInteger> iter = bigRange.withDiff(1);
     BigInteger count = BigInteger.valueOf(0);
     for (final BigInteger i : iter) {
@@ -17,7 +17,7 @@ public class bigRangeTest {
     }
   }
 
-  @SuppressWarnings({ "static-method", "static-access" }) @Test public void withDiff3Only() {
+  @SuppressWarnings({ "static-method" }) @Test public void withDiff3Only() {
     final Iterable<BigInteger> iter = bigRange.withDiff(3);
     BigInteger count = BigInteger.valueOf(0);
     for (final BigInteger i : iter) {
@@ -29,7 +29,7 @@ public class bigRangeTest {
     }
   }
 
-  @SuppressWarnings({ "static-method", "static-access" }) @Test public void fromAndWithDiff() {
+  @SuppressWarnings({ "static-method" }) @Test public void fromAndWithDiff() {
     final Iterable<BigInteger> iter = bigRange.from(2).withDiff(3);
     BigInteger count = BigInteger.valueOf(2);
     for (final BigInteger i : iter) {
@@ -77,4 +77,37 @@ public class bigRangeTest {
      azzert.assertEquals(BigInteger.valueOf(18),bigRange.from(3).to(9).withDiff(3).Sum());
    }
   
+   
+   @SuppressWarnings({ "static-method", "static-access" }) @ Test
+   public void impossibleRangeNegative() {
+     azzert.assertEquals(null, bigRange.to(-5).withDiff(1));
+   }
+   
+   @SuppressWarnings({ "static-method", "static-access" }) @Test
+   public void impossibleRangePossitive() {
+     azzert.assertEquals(null, bigRange.from(2).to(5).withDiff(-1));
+   }
+   
+   @SuppressWarnings({ "static-method" }) @Test 
+   public void naturals() {
+     BigInteger count = BigInteger.valueOf(0);
+     for (final BigInteger i : bigRange.naturals()) {
+       Assert.assertEquals(count, i);
+       count.add(BigInteger.valueOf(1));
+       if (count.equals(BigInteger.valueOf(1000)))
+         break;
+       count = count.add(BigInteger.valueOf(1));
+     }
+   }
+   
+   @SuppressWarnings({ "static-access", "static-method" }) @Test
+   public void bigIntegersRange() {
+       Iterable<BigInteger> iter=bigRange.from(BigInteger.valueOf(5)).to(BigInteger.valueOf(123)).withDiff(BigInteger.valueOf(3));
+       BigInteger count=BigInteger.valueOf(5);
+       for(BigInteger i : iter) {
+         azzert.assertEquals(count,i);
+         count.add(BigInteger.valueOf(1));
+         count=count.add(BigInteger.valueOf(3));
+       }
+   }
 }
