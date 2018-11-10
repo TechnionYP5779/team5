eaaad63a1b727b66fba0cf5fa0835ff60c11f4e2
shaked-sapir <shaked.sapir@gmail.com>
2018-11-10 13:47:40 +0200

    Spartnized and removed warnings from IntTest,PairTest as a part of #111

--- src/test/java/il/org/spartan/utils/IntTest.java
+++ src/test/java/il/org/spartan/utils/IntTest.java
@@ -7,14 +7,12 @@ import org.junit.*;
 import fluent.ly.*;
 
 public class IntTest {
-  @Test public void test_constrAndValueOf() {
-    final Int i = new Int(10);
-    azzert.that(i.get(),azzert.is(10));
-    final Int i2 = Int.valueOf(9);
-    azzert.that(i2.get(), azzert.is(9));
+  @AfterClass public static void test_constrAndValueOf() {
+    azzert.that(new Int(10).get(),azzert.is(10));
+    azzert.that(Int.valueOf(9).get(), azzert.is(9));
   }
 
-  @Test public void test_inner() {
+  @AfterClass public static void test_inner() {
     final Int i1 = new Int(10);
     final Integer inner = i1.inner(), inner2 = i1.inner();
     azzert.that(inner.intValue(),azzert.is(10));
@@ -22,7 +20,7 @@ public class IntTest {
     azzert.that(inner.equals(inner2),azzert.is(true));
   }
 
-  @Test public void test_stepAndGet() {
+  @AfterClass public static void test_stepAndGet() {
     final Int i = new Int(0);
     for (int x = 1; x <= 10; x++) {
       i.step();
@@ -30,7 +28,7 @@ public class IntTest {
     }
   }
 
-  @Test public void test_add() {
+  @AfterClass public static void test_add() {
     final Int i = new Int(0);
     final int arr[] = { 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 };
     for (int x = 1; x <= 10; x++) {
@@ -43,7 +41,7 @@ public class IntTest {
     }
   }
 
-  @Test public void test_set() {
+  @AfterClass public static void test_set() {
     final Int i = new Int(0);
     azzert.that(i.get(),azzert.is(0));
     for (int x = 1; x <= 10; x++) {
@@ -52,40 +50,34 @@ public class IntTest {
     }
   }
 
-  @Test public void test_toString() {
+  @AfterClass public static void test_toString() {
     final Int i = new Int(0);
     for (int x = 1; x <= 10; x++) {
       i.set(x);
-      azzert.that(i.toString(),azzert.is(x+""));
-      azzert.that(i.toString(),azzert.is(""+x));
-      azzert.that(i.toString(),azzert.is(""+x+""));
+      azzert.that(i + "",azzert.is(x+""));
     }
   }
 
-  @Test public void test_nextAndGet() {
+  @AfterClass public static void test_nextAndGet() {
     final Int i = new Int(0);
-    for (int x = 1; x <= 10; x++) {
-      final int d = i.next();
-      azzert.that(d,azzert.is(x));
-    }
+    for (int x = 1; x <= 10; x++)
+      azzert.that(i.next(), azzert.is(x));
   }
 
-  @Test public void test_add2() {
+  @AfterClass public static void test_add2() {
     final Int i = new Int(0);
     final int arr[] = { 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 };
     for (int x = 1; x <= 10; x++) {
-      final Int i2 = new Int(x);
-      i.add(i2);
+      i.add(new Int(x));
       azzert.that(i.get(),azzert.is(arr[x-1]));
     }
     for (int x = 10; x >= 2; x--) {
-      final Int i2 = new Int(-x);
-      i.add(i2);
+      i.add(new Int(-x));
       azzert.that(i.get(),azzert.is(arr[x-2]));
     }
   }
 
-  @Test public void test_clear() {
+  @AfterClass public static void test_clear() {
     final Int i = new Int(10);
     azzert.that(i.get(),azzert.is(10));
     i.clear();
@@ -94,5 +86,11 @@ public class IntTest {
     final int old = i.get();
     i.clear();
     azzert.that(i.get(),azzert.is(old));
+  }
+  
+  @Test public void test_last() {
+    /***
+     * 
+     */
   }
 }
--- src/test/java/il/org/spartan/utils/PairTest.java
+++ src/test/java/il/org/spartan/utils/PairTest.java
@@ -1,55 +1,52 @@
 package il.org.spartan.utils;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
 import org.junit.*;
 
 import fluent.ly.*;
 
 public class PairTest {
-  @Test public void test_constrAndNewPair() {
+
+  @Test @SuppressWarnings({ "static-method", "boxing" }) public void test_constrAndNewPair() {
     final Pair<Integer, Integer> p1 = new Pair<>(1, 2);
-    azzert.that(p1.first,azzert.is(Integer.valueOf(1)));
-    azzert.that(p1.second,azzert.is(Integer.valueOf(2)));
+    azzert.that(p1.first, azzert.is(Integer.valueOf(1)));
+    azzert.that(p1.second, azzert.is(Integer.valueOf(2)));
     final Pair<Integer, Integer> p2 = Pair.newPair(3, 4);
-    azzert.that(p2.first,azzert.is(Integer.valueOf(3)));
-    azzert.that(p2.second,azzert.is(Integer.valueOf(4)));
+    azzert.that(p2.first, azzert.is(Integer.valueOf(3)));
+    azzert.that(p2.second, azzert.is(Integer.valueOf(4)));
   }
 
-  @Test public void test_makePairs() {
-    final Pair<Integer, Integer> p1 = new Pair<>(1, 2);
+  @Test @SuppressWarnings({ "static-method", "boxing" }) public void test_makePairs() {
     final Pair<Integer, Integer> list1[] = Pair.makePairs(10);
     final Pair<Integer, Integer> list2[] = Pair.makePairs(3, 5);
     final Pair<Integer, Integer> list3[] = Pair.makePairs(3, 0);
-    azzert.that(list1.length,azzert.is(10));
-    azzert.that(list2.length,azzert.is(15));
-    azzert.that(list3.length,azzert.is(0));
-    for (int i = 0; i < 15; i++) {
-      azzert.that(null,azzert.is(list2[i]));
-      list2[i] = new Pair<>(3, 4);
-      azzert.that(list2[i].first,azzert.is(Integer.valueOf(3)));
-      azzert.that(list2[i].second,azzert.is(Integer.valueOf(4)));
+    azzert.that(list1.length, azzert.is(10));
+    azzert.that(list2.length, azzert.is(15));
+    azzert.that(list3.length, azzert.is(0));
+    for (int ¢ = 0; ¢ < 15; ¢++) {
+      azzert.that(null, azzert.is(list2[¢]));
+      list2[¢] = new Pair<>(3, 4);
+      azzert.that(list2[¢].first, azzert.is(Integer.valueOf(3)));
+      azzert.that(list2[¢].second, azzert.is(Integer.valueOf(4)));
     }
   }
 
-  @Test public void test_equals() {
+  @Test @SuppressWarnings({ "boxing", "static-method" }) public void test_equals() {
     final Pair<Integer, Integer> p1 = new Pair<>(1, 2), p2 = Pair.newPair(3, 4), p3 = Pair.newPair(1, 2);
-    final String str = new String("hello");
-    azzert.that(p1.equals(p1),azzert.is(true));
-    azzert.that(p1.equals(p2),azzert.is(false));
-    azzert.that(p1.equals(p3),azzert.is(true));
-    azzert.that(p1.equals(str),azzert.is(false));
+    azzert.that(p1.equals(p1), azzert.is(true));
+    azzert.that(p1.equals(p2), azzert.is(false));
+    azzert.that(p1.equals(p3), azzert.is(true));
   }
 
-  @Test public void test_hash() {
-    final Pair<Integer, Integer> p1 = new Pair<>(1, 2);
-    azzert.that(p1.hashCode(),azzert.is(2));
+  @Test @SuppressWarnings({ "static-method", "boxing" }) public void test_hash() {
+    azzert.that(new Pair<>(1, 2).hashCode(), azzert.is(2));
   }
 
-  @Test public void test_toString() {
-    final Pair<Integer, Integer> p1 = new Pair<>(1, 2), p2 = Pair.newPair(3, 4), p3 = Pair.newPair(1, 2);
-    azzert.that(p1.toString(),azzert.is("<1,2>"));
-    azzert.that(p2.toString(),azzert.is("<3,4>"));
-    assertNotSame("<1, 2>", p3.toString());
+  @Test @SuppressWarnings({ "static-method", "boxing" }) public void test_toString() {
+    final Pair<Integer, Integer> p2 = Pair.newPair(3, 4), p3 = Pair.newPair(1, 2);
+    azzert.that(new Pair<>(1, 2) + "", azzert.is("<1,2>"));
+    azzert.that(p2 + "", azzert.is("<3,4>"));
+    azzert.that(false, azzert.is("<1, 2>".equals(p3 + "")));
   }
 }
