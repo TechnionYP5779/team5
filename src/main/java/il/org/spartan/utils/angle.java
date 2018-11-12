57102e11ef86de5f406062f657fc0802a428c5fe
LiorBenYamin <lior.benyamin7@gmail.com>
2018-11-12 21:39:44 +0200

    finished check box 3 in #96. worked 15 minutes

--- src/main/java/il/org/spartan/utils/angle.java
+++ src/main/java/il/org/spartan/utils/angle.java
@@ -2,12 +2,15 @@ package il.org.spartan.utils;
 
 public class angle {
   
+  public static final angle pi = new angle(180);
+  private static final double deg2Rad = Math.PI / 180;
+  private static final double rad2Deg = 180 / Math.PI;
   private double deg;
   private double rad;
 
   public angle(double i) {
-    deg = 20;
-    rad = 1;
+    deg = i;
+    rad = i * deg2Rad;
   }
   
   public double getDegrees() {
@@ -19,10 +22,10 @@ public class angle {
   }
 
   public static angle degrees(double i) {
-    return new angle(20);
+    return new angle(i);
   }
 
   public static angle radians(double i) {
-    return new angle(1);
+    return new angle(i * rad2Deg);
   }
 }
--- src/test/java/il/org/spartan/utils/angleTest.java
+++ src/test/java/il/org/spartan/utils/angleTest.java
@@ -9,6 +9,7 @@ public class angleTest {
   @Test public void angleCreate() {
     azzert.that(angle.degrees(20).getDegrees(), azzert.is((double)20));
     azzert.that(angle.radians(1).getRadians(), azzert.is((double)1));
+    azzert.that(angle.pi.getRadians(), azzert.is(Math.PI));
   }
 }
 
