8b8f1a1f8b7026223e3f1ef602dca1a3a172fdfe
shaked-sapir <shaked.sapir@gmail.com>
2018-11-14 02:25:17 +0200

    iteration over pairs sorted by: implemented ascending and descending sorts, need to implement iteration. issue #83

--- src/main/java/il/org/spartan/utils/PairsList.java
+++ src/main/java/il/org/spartan/utils/PairsList.java
@@ -3,17 +3,50 @@ package il.org.spartan.utils;
 import java.util.*;
 
 public class PairsList {
-  
-  public ArrayList<Pair<Double,Double>> l;
+  @SuppressWarnings({ "rawtypes", "hiding" }) public class PAIR<Double,Double1> implements Comparable{
+    public Pair<Double,Double> p;
+    
+    @SuppressWarnings("unchecked") public PAIR(Pair<Double,Double1> np) {
+      p=(Pair<Double, Double>) np;
+    }
+
+    @SuppressWarnings({ "cast", "unchecked", "null" }) @Override public int compareTo(Object o) {
+      Double $ = (Double) ((PAIR<Double, Double>) o).p.first;
+      if (((Comparable) (Double) p.first).compareTo($) != 0)
+        return ((Comparable) (Double) p.first).compareTo($);
+      return ((Comparable) (Double) p.second).compareTo((Double) ((PAIR<Double, Double>) o).p.second); 
+    }
+  }
+  public ArrayList<PAIR<Double,Double>> l;
   public PairsList() {
     l = new ArrayList<>();
   }
 
   @SuppressWarnings("boxing") public void record(double d, double e) {
-    l.add(new Pair<>(d,e));
+    l.add(new PAIR<>(new Pair<>(d,e)));
   }
 
   @SuppressWarnings("boxing") public boolean contains(double d, double e) {
-    return l.contains(new Pair<>(d,e));
+    Pair<Double,Double> t = new Pair<>(d,e);
+    for (PAIR<Double,Double> x : l)
+      if (x.p.equals(t))
+        return true;
+    return false;
+  }
+
+  public void sortAsc() {
+    Collections.sort(l,new Comparator<PAIR<Double,Double>>(){
+      @Override public int compare(PAIR<Double,Double> d, PAIR<Double,Double> e) {
+        return d.compareTo(e);
+      }
+    });
+  }
+  
+  public void sortDesc() {
+    Collections.sort(l,new Comparator<PAIR<Double,Double>>(){
+      @Override public int compare(PAIR<Double,Double> d, PAIR<Double,Double> e) {
+        return -d.compareTo(e);
+      }
+    });
   }
 }
--- src/test/java/il/org/spartan/utils/PairsListTest.java
+++ src/test/java/il/org/spartan/utils/PairsListTest.java
@@ -5,9 +5,22 @@ import org.junit.*;
 import fluent.ly.*;
 
 public class PairsListTest {
-  @SuppressWarnings("static-method") @Test public void test() {
+  @SuppressWarnings("static-method") @Test public void record() {
     PairsList p = new PairsList();
     p.record(1.1,2.2);
+    System.out.println(p.l.get(0).p + "");
     azzert.that(p.contains(1.1,2.2), azzert.is(true));
+  }
+  
+  @SuppressWarnings({ "static-method", "boxing" }) @Test public void sort() {
+    PairsList p = new PairsList();
+    for(int ¢=0;¢<10;¢++)
+      p.record(10.0-¢, 20.0-¢);
+    p.sortAsc();
+    for(int ¢=1;¢<=10;¢++)
+      azzert.that(p.l.get(¢ - 1).p.equals(new Pair<>(1. * ¢, 1. * (¢ + 10))), azzert.is(true));
+    p.sortDesc();
+    for(int ¢=0;¢<10;¢++)
+      azzert.that(p.l.get(¢).p.equals(new Pair<>(10.0 - ¢, 20.0 - ¢)), azzert.is(true));
   }
 }
