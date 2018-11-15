daa1118e2088b958c9d3b81ddb1028b3d14e8d3f
shaked-sapir <shaked.sapir@gmail.com>
2018-11-15 12:00:32 +0200

    checked it in all -tests-, seemed to be OK, please re-check me before closing #130

--- src/test/java/fluent/ly/countTest.java
+++ src/test/java/fluent/ly/countTest.java
@@ -5,12 +5,12 @@ import java.util.stream.*;
 
 import org.junit.*;
 
-public class countTest {
-  @Test @SuppressWarnings({ "static-method", "rawtypes", "unchecked" }) public void emptyIterableTest() {
+@SuppressWarnings("static-method") public class countTest {
+  @Test @SuppressWarnings({"rawtypes", "unchecked"}) public void emptyIterableTest() {
     azzert.assertEquals(0, count.of(new ArrayList()));
   }
 
-  @Test @SuppressWarnings("static-method") public void Count100ObjectsTest() {
+  @Test public void Count100ObjectsTest() {
     azzert.assertEquals(100, count.of(Stream.generate(Object::new).limit(100).collect(Collectors.toList())));
   }
 }
