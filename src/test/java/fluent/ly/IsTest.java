6149db8f23ef709c5bf37e471ddc0bfbbe3339e2
NirChachmovitz <niro1001@gmail.com>
2018-11-19 12:45:59 +0200

    added a test for class is.java

--- src/test/java/fluent/ly/IsTest.java
+++ src/test/java/fluent/ly/IsTest.java
@@ -1,66 +1,76 @@
 package fluent.ly;
 
+
 import java.util.*;
 
 import org.junit.*;
 
-@SuppressWarnings("static-method") public class IsTest {
-  @Test @SuppressWarnings("null") public void testIn() {
-    final Integer a = null;
+
+
+/**
+ * @since 2018-11-14
+ * @author Nir Chachamovitz
+ * Tests for class is, checks several properties on a set of objects
+ */
+@SuppressWarnings({"boxing", "null", "static-method"})
+public class IsTest {
+   @Test public void testIn() {
+    Integer a = null;
     azzert.assertEquals(true, is.in("", ""));
     azzert.assertEquals(true, is.in("nir", "igor", "shaked", "shalev", "lior", "or", "nir"));
-    azzert.assertEquals(false, is.in(box.it(1), a));
-    azzert.assertEquals(false, is.in(box.it(1), box.it(3), box.it(8), box.it(5)));
+    azzert.assertEquals(false, is.in((Integer)1, a));
+    azzert.assertEquals(false, is.in(1, 3, 8, 5));
     azzert.assertEquals(false, is.in(""));
   }
-
-  @Test public void testNot() {
+  
+  @Test public void testNot(){
     azzert.assertEquals(false, is.not.in("", ""));
     azzert.assertEquals(false, is.not.in("nir", "igor", "shaked", "shalev", "lior", "or", "nir"));
-    azzert.assertEquals(true, is.not.in(box.it(1), box.it(3), box.it(8), box.it(5)));
+    azzert.assertEquals(true, is.not.in(1, 3, 8, 5));
     azzert.assertEquals(true, is.not.in(""));
   }
-
+  
   @Test public void testOut() {
     azzert.assertEquals(false, is.out("", ""));
     azzert.assertEquals(false, is.out("nir", "igor", "shaked", "shalev", "lior", "or", "nir"));
-    azzert.assertEquals(true, is.out(box.it(1), box.it(3), box.it(8), box.it(5)));
+    azzert.assertEquals(true, is.out(1, 3, 8, 5));
     azzert.assertEquals(true, is.out(""));
   }
-
-  @Test public void testIsInInt() {
-    final int num = 4;
-    azzert.assertEquals(true, is.intIsIn(num, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-    azzert.assertEquals(false, is.intIsIn(num, 1, 2, 3, 5, 6, 7, 8, 9, 10));
-  }
-
+  
+//  @Test public static void testIsInInt() {
+//    int num = 4;
+//    azzert.assertEquals(true, is.intIsIn(num, 1,2,3,4,5,6,7,8,9,10));
+//    azzert.assertEquals(false, is.intIsIn(num, 1, 2, 3, 5, 6, 7, 8, 9, 10));
+//  }
+  
   @Test public void testIsEmptyNull() {
-    final String s = null;
-    final Object a[] = null;
-    final Iterable<Object> it = null;
-    final Collection<Object> coll = null;
+    String s = null;
+    Object a[] = null;
+    Iterable<Object> it = null;
+    Collection<Object> coll = null;
     azzert.assertEquals(true, is.empty(s));
     azzert.assertEquals(true, is.empty(a));
     azzert.assertEquals(true, is.empty(it));
     azzert.assertEquals(true, is.empty(coll));
+    
   }
-
+  
   @Test public void testIsEmpty() {
-    final String s = "";
-    final Object a[] = {};
-    final Iterable<Object> it = new ArrayList<>();
-    final Collection<Object> coll = new ArrayList<>();
+    String s = "";
+    Object a[] = {};
+    Iterable<Object> it = new ArrayList<>();
+    Collection<Object> coll = new ArrayList<>();
     azzert.assertEquals(true, is.empty(s));
     azzert.assertEquals(true, is.empty(a));
     azzert.assertEquals(true, is.empty(it));
     azzert.assertEquals(true, is.empty(coll));
   }
-
+  
   @Test public void testIsNotEmpty() {
-    final String s = "Hello World";
-    final Integer a[] = { box.it(1), box.it(2), box.it(3), box.it(4) };
-    final ArrayList<Integer> it = new ArrayList<>();
-    it.add(box.it(1));
+    String s = "Hello World";
+    Integer a[] = {1,2,3,4};
+    ArrayList<Integer> it = new ArrayList<>();
+    it.add(1);
     azzert.assertEquals(false, is.empty(s));
     azzert.assertEquals(false, is.empty(a));
     azzert.assertEquals(false, is.empty(it));
