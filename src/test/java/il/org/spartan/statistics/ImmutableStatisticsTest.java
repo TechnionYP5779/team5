6b19df5fd750970e4625e4e8f0b3139cdeaf557b
NirChachmovitz <niro1001@gmail.com>
2018-11-20 22:07:33 +0200

    closes #159

---
+++ src/test/java/il/org/spartan/statistics/ImmutableStatisticsTest.java
@@ -0,0 +1,39 @@
+/** add here documentation for file
+ * @author Fname Sname
+ * @since year-month-day */
+package il.org.spartan.statistics;
+
+import static il.org.spartan.bench.Unit.*;
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import fluent.ly.*;
+
+/** @author Nir Chachamovitz
+ * @since 2018-11-20 */
+public class ImmutableStatisticsTest {
+  @Test public void flippingTest() {
+    azzert.that(new ImmutableStatistics() {
+      static final long serialVersionUID = 1;
+    }.flipping(), azzert.is(Double.NaN));
+  }
+  
+  @Test public void madTest() {
+    new ImmutableStatistics() {
+      static final long serialVersionUID = 1;
+    }.all();
+  }
+  
+//  @Test public void formatTest() {
+//    new ImmutableStatistics() {
+//      static final long serialVersionUID = 1;
+//    }.mad();
+//  }
+
+  @Test public void unitTest() {
+    azzert.isNull(new ImmutableStatistics() {
+      static final long serialVersionUID = 1;
+    }.unit());
+  }
+}
