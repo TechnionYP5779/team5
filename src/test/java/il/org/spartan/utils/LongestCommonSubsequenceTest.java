db9e49210ca7831a81c9a225aaebb13ced5c1726
igorbash <igor731996@gmail.com>
2018-11-21 12:08:15 +0200

    close #168

---
+++ src/test/java/il/org/spartan/utils/LongestCommonSubsequenceTest.java
@@ -0,0 +1,23 @@
+/**add here documentation for file 
+ * @author Fname Sname
+ * @since year-month-day */
+package il.org.spartan.utils;
+
+import static fluent.ly.azzert.is;
+import org.junit.*;
+
+import fluent.ly.*;
+
+/**
+ * @author Igor Bashmashnikov
+ *
+ */
+@SuppressWarnings("static-method") 
+public class LongestCommonSubsequenceTest {
+  @Test public void testSimilarity() {
+    String str1 = "abcdefg";
+    String str2 = "bcde";
+    LongestCommonSubsequence lcs = new LongestCommonSubsequence(str1, str2);
+    azzert.that(lcs.similarity(), is(0.7272727272727273));
+  }
+}
