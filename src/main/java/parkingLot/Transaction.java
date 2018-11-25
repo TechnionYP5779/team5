412985efeeabe008d2a2c3ad1a0f423bdb2cb86c
LiorBenYamin <lior.benyamin7@gmail.com>
2018-11-25 22:04:38 +0200

    add package parkingLot

---
+++ src/main/java/parkingLot/Transaction.java
@@ -0,0 +1,12 @@
+/**add here documentation for file 
+ * @author Fname Sname
+ * @since year-month-day */
+package parkingLot;
+
+/**
+ * @author Lior
+ *
+ */
+public class Transaction {
+  int id;
+}
