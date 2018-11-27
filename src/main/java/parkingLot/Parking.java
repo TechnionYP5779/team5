a56d7f84f75214f5131d90ea316d015e093cf1df
shaked-sapir <shaked.sapir@gmail.com>
2018-11-27 17:22:54 +0200

    #171 : defined class fields

--- src/main/java/parkingLot/Parking.java
+++ src/main/java/parkingLot/Parking.java
@@ -1,14 +1,34 @@
 /**add here documentation for file 
- * @author Fname Sname
- * @since year-month-day */
+ * @author Shaked Sapir, Shalev Kuba
+ * @since 2018-11-27 */
 package parkingLot;
 
+import java.util.*;
+
 /**
- * @author Shaked Sapir
+ * @author Shaked Sapir, Shalev Kuba
  *
  */
 public class Parking {
-  /**
-   * 
-   */
+  int id;
+  User owner;
+  enum size{
+    PRIVATE_CAR, MOTORCYCLE, TRANSIT, BUS 
+  } 
+  
+  public class AvailableSlot{
+    Date from;
+    Date to;
+    double price_for_hour;
+    
+    public AvailableSlot(Date _from, Date _to, double price) {
+      this.from = _from;
+      this.to = _to;
+      this.price_for_hour = price;
+    }
+  }
+  
+  /** Format: State, city, street, apratment no. **/
+  String location;
+  List<AvailableSlot> availableSlots;
 }
