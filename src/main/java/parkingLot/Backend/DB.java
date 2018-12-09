266bd9206015b30f25af2c6913da297a205473dd
orfeld415 <orfeldman@campus.technion.ac.il>
2018-12-10 00:42:44 +0200

    creating basic DB interface

---
+++ src/main/java/parkingLot/Backend/DB.java
@@ -0,0 +1,13 @@
+package parkingLot.Backend;
+import java.util.ArrayList;
+import parkingLot.Logic.Parking;
+import parkingLot.Logic.User;
+
+public interface DB {
+	
+	User getUser(String id, String password);
+	boolean addUser(User u);
+	ArrayList<Parking> getParkings();
+	boolean addParking(Parking p);
+	
+}
