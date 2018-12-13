02b5094e0090193b3825662ea0153c162cda3e24
igorbash <igor731996@gmail.com>
2018-12-13 03:13:23 +0200

    updated spring version and deleted unnecessary class

--- src/main/java/parkingLot/Backend/WebApplication.java
+++ src/main/java/parkingLot/Backend/WebApplication.java
@@ -2,8 +2,6 @@ package parkingLot.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,15 +9,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 public class WebApplication {
-	@Bean
-	@SuppressWarnings("static-method")
-	public FilterRegistrationBean jwtFilter() {
-		final FilterRegistrationBean $ = new FilterRegistrationBean();
-		$.setFilter(new JwtFilter());
-		$.addUrlPatterns("/api/*");
-		return $;
-	}
-
 	public static void main(final String[] args) throws Exception {
 		SpringApplication.run(WebApplication.class, args);
 	}
